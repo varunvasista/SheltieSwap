77b006dbc7b19bfa808130bf57c64689d8655b10
varunvasista <varunvashista@engineer.com>
2021-05-21 04:04:46 +0530

    Updates

--- src/utils/constructSameAddressMap.ts
+++
@@ -1,9 +0,0 @@
-export function constructSameAddressMap<T extends string>(address: T): { [chainId: number]: T } {
-  return {
-    [1]: address,
-    [3]: address,
-    [42]: address,
-    [4]: address,
-    [5]: address,
-  }
-}
